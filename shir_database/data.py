b4ec3ff85ce922d96cd896c89052d485b5e35f39
حصین <sh.danishyar@gmail.com>
2020-09-09 11:02:24 +0430

    database fineshed

--- shir_database/data.py
+++ shir_database/data.py
@@ -47,3 +47,250 @@ for name in shirdb:
 
 database.close()
 
+
+import  sqlite3
+database = sqlite3.connect('shirdb.db')
+cursor = database.cursor()
+
+query = ''' SELECT BUYER_NAME FROM shirdb3  LIMIT 3 OFFSET 4'''
+cursor.execute(query)
+
+shirdb = cursor.fetchall()
+
+
+for name in shirdb:
+    print(name)
+
+database.close()
+
+import  sqlite3
+database = sqlite3.connect('shirdb.db')
+cursor = database.cursor()
+
+
+# select the id in mentioned here
+#query = ''' SELECT BUYER_NAME FROM shirdb3 WHERE ID IN(3,5,1)'''
+#query = ''' SELECT BUYER_NAME FROM shirdb3 WHERE PRICE>100'''
+#query = ''' SELECT BUYER_NAME FROM shirdb3 WHERE ID NOT IN(3,1)'''
+# show only ends with 'r', or 'os'
+#query = ''' SELECT BUYER_NAME FROM shirdb3 WHERE BUYER_NAME LIKE '%r' '''
+#query = ''' SELECT BUYER_NAME FROM shirdb3 WHERE BUYER_NAME LIKE '%os' '''
+# show the record if you see  's' in table anywhere you see 's'
+query = ''' SELECT BUYER_NAME FROM shirdb3 WHERE BUYER_NAME LIKE '%s%' '''
+cursor.execute(query)
+
+shirdb = cursor.fetchall()
+
+
+for name in shirdb:
+    print(name)
+
+database.close()
+
+
+
+
+
+
+
+
+import  sqlite3
+database = sqlite3.connect('shirdb.db')
+cursor = database.cursor()
+
+#delete a row in database
+query = ''' DELETE FROM shirdb3 WHERE BUYER_NAME LIKE '%tr$' '''
+cursor.execute(query)
+
+shirdb = cursor.fetchall()
+
+for name in shirdb:
+    print(name)
+
+database.close()
+
+
+
+
+import sqlite3
+
+db = sqlite3.connect('shirdb.db')
+cursor = db.cursor()
+
+
+
+# if you have dublicate row or dublicate row data, so you wanna have only one of them 
+query = 'SELECT DISTINCT BUYER_NAME FROM shirdb3'
+cursor.execute(query)
+
+name_of_people = cursor.fetchall()
+
+
+for name in name_of_people:
+    print(name)
+
+
+db.commit()
+db.close()
+
+
+
+
+import sqlite3
+
+db = sqlite3.connect('shirdb.db')
+
+cursor = db.cursor()
+
+#block operator 
+#block operator is very kind of semllar to 'like' operator but unlike to block operator is case sensetive and block operator is case sensetive 
+
+# star '*'  it meanse that all but question mark '?' match only one carrector
+
+#query = ''' SELECT BUYER_NAME FROM shirdb3 WHERE BUYER_NAME GLOB 's*' '''
+#it means that after 'rk' any number of carrector can be but befor only one currector is allowed
+#query = ''' SELECT BUYER_NAME FROM shirdb3 WHERE BUYER_NAME GLOB '?ir*' '''
+#print the name which has number inside 
+#query = ''' SELECT BUYER_NAME FROM shirdb3 WHERE BUYER_NAME GLOB '*[1-9]*' '''
+# it means that give me the name which start with A,B,C,D
+query = ''' SELECT BUYER_NAME FROM shirdb3 WHERE BUYER_NAME GLOB '[A-D]*' '''
+cursor.execute(query)
+
+names = cursor.fetchall()
+
+for name in names:
+    print(name)
+
+
+
+
+
+import sqlite3
+
+db = sqlite3.connect('student_records.db')
+cursor = db.cursor()
+
+#IF YOU  wanna find the union between two table like to 'set' so you can do as follows
+#query = ''' SELECT dept_name FROM 'std table' UNION SELECT dept_name FROM teacher_table'''
+#intersect two table is like intersecting two set 
+query = ''' SELECT dept_name FROM 'std table' INTERSECT SELECT dept_name FROM teacher_table'''
+cursor.execute(query)
+
+records= cursor.fetchall()
+
+for record in records:
+    print(record)
+
+db.commit()
+db.close()
+
+
+
+import sqlite3
+
+db = sqlite3.connect('shirdb.db')
+cursor = db.cursor()
+
+
+#math in sqlite3
+
+#query = '''SELECT random()'''
+#select rounded value with '2' dicmal places
+query = ''' SELECT round(98.939434,2)'''
+cursor.execute(query)
+
+
+random_number = cursor.fetchall()
+
+for random in random_number:
+    print(random)
+
+# it will give us random value between '+- 922337203685477808'
+
+
+
+
+
+import sqlite3
+
+db = sqlite3.connect('student_records.db')
+
+cursor = db.cursor()
+#agregation in sqlite3
+# query = '''SELECT AVG(marks) '''
+#query = ''' SELECT AVG(marks) FROM students WHERE ID<8'''
+
+# if you wanna distinct the mark wich has the same value
+#query = '''SELECT AVG(DISTINCT mark) FROM students '''
+
+# if you wanna count 
+#query = '''SELECT COUNT(*) FROM students'''
+
+
+#if you wanna find the uniqe one 
+#query = '''SELECT COUNT(DISTINCT marks) FROM students'''
+#query = '''SELECT COUNT(DISTINCT marks) FROM students WHERE LIKE "s%" '''
+
+#find the maximum/minmum 'id'
+#query = '''SELECT MAX(ID) FROM studnets'''
+#query = '''SELECT SUM(ID) FROM students'''
+
+
+#find absolute value --> if we have any nigative vlaue with absolute we can convert to positive integer filed 
+#query = '''SELECT ABS(marks) FROM students'''
+cursor.execute(query)
+
+marks = cursor.fetchall()
+for mark in marks:
+    print(mark)
+
+
+
+import sqlite3
+
+db = sqlite3.connect('shirdb.db')
+
+cursor = db.cursor()
+
+
+#string --> substring --- here i wanna print the string that word wich start from two it means that starting from 'h' 
+#query = '''SELECT substr('Shir',2)'''
+
+#next time i wanna start from '2' and just print '5' character
+#query = '''SELECT substr('jef bezos',2,5)'''
+#query = '''SELECT substr('jef bezos',-1,5)'''
+
+
+#if you wanna remove some character from the start and end of character, you should use 'Trim'
+
+#query = '''SELECT Trim('rshir','r')'''
+
+
+# if you wannasome spaces 
+#query = '''SELECT Trim(' shir') '''
+
+#if you wanna remove from left of word 'LTrim', but right is 'Rtrim'
+#query = ''' SELECT LTrim('####shir###','#')'''
+
+#making upercase or lowercase 
+#query = '''SELECT Upper('shir')'''
+
+query = '''SELECT Lower('SHIR')'''
+records = cursor.execute(query)
+
+for record in records:
+    print(record)
+
+db.commit()
+db.close()
+
+
+
+
+
+
+
+
+
+
+
